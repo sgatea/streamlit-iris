5e54040779a525859cc638a6b70b2e9116ae9b47
sgatea <32777693+sgatea@users.noreply.github.com>
2022-08-06 14:37:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from prediction import predict
 st.title('Classifying Iris Flowers')
 st.markdown('Toy model to play to classify iris flowers into \
      (setosa, versicolor, virginica) based on their sepal/petal \
-    and length/width.')
+    and length/width using base RandomForestClassifier.')
 
 st.header("Plant Features")
 col1, col2 = st.columns(2)
